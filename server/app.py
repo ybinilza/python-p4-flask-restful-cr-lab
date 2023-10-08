4a5459f71ba146186a7bf0e14bcc832662e092c1
Binil Thomas Zachariah <binilreni7@gmail.com>
2023-10-08 00:05:43 -0400

    Lab completed

--- server/app.py
+++ server/app.py
@@ -17,10 +17,55 @@ db.init_app(app)
 api = Api(app)
 
 class Plants(Resource):
-    pass
+    
+    def get(self):
+        
+        response_dict_list = [plant.to_dict() for plant in Plant.query.all()]
+
+        response = make_response(
+            response_dict_list,
+            200
+        )
+
+        return response
+    
+    def post(self):
+
+        plant = request.get_json()
+
+        new_plant = Plant(
+            name=plant["name"],
+            image=plant["image"],
+            price=plant["price"],
+        )
+
+        db.session.add(new_plant)
+        db.session.commit()
+
+        response_dict = new_plant.to_dict()
+
+        response = make_response(
+            response_dict,
+            201,
+        )
+
+        return response
+    
+api.add_resource(Plants, '/plants')
 
 class PlantByID(Resource):
-    pass
+    
+    def get(self, id):
+
+        response_dict = Plant.query.filter_by(id=id).first().to_dict()
+
+        response = make_response(
+            response_dict,
+            200,
+        )
+        return response
+
+api.add_resource(PlantByID, '/plants/<int:id>')
         
 
 if __name__ == '__main__':
